84ba875eccf5a71bfa9f4305f05d25cd966a60f9
hassanalee <hassanali14563@gmail.com>
2024-02-23 16:05:50 +0500

    created api to delete note

--- api/controllers/note.controllers.js
+++ api/controllers/note.controllers.js
@@ -9,4 +9,21 @@ const createNote = async (req, res) => {
     res.status(500).send(error);
   }
 };
-module.exports = { createNote };
+// delete note
+const deleteNote = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await Note.findById(id);
+    if (!note) {
+      return res.status(404).send("Note not found");
+    }
+    if (note.userId != req.user.id) {
+      return res.status(401).send("You can only delete your own note");
+    }
+    const deletedNote = await Note.findByIdAndDelete(id);
+    res.status(200).send("Note deleted successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+module.exports = { createNote, deleteNote };
--- api/routes/note.routes.js
+++ api/routes/note.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const verifyToken = require("../utils/verifyToken.js");
-const { createNote } = require("../controllers/note.controllers.js");
+const {
+  createNote,
+  deleteNote,
+} = require("../controllers/note.controllers.js");
 router.post("/create-note", verifyToken, createNote);
+router.delete("/delete-note/:id", verifyToken, deleteNote);
 module.exports = router;
