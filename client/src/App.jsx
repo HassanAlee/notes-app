87621dc5980f36f59044da280b0c0e8ef420abdf
hassanalee <hassanali14563@gmail.com>
2024-02-27 15:12:49 +0500

    added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 const App = () => {
+  const pathName = window.location.pathname;
+  console.log(pathName);
   return (
-    <div>App</div>
+    <>
+      {pathName != "/auth" && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+      </Routes>
+    </>
   )
 }
 
